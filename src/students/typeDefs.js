34c175adafbd00e0cbb43a88d3e6a5c811dac28a
José Nieto <jdnv.123@gmail.com>
2018-11-04 00:51:54 -0500

    First commit

---
+++ src/students/typeDefs.js
@@ -0,0 +1,23 @@
+export const StudentsTypeDef = `
+type Student {
+    code: Int!
+    name: String!
+    credits: Int!
+    professor: String!
+}
+input StudentInput {
+    name: String!
+    credits: Int!
+    professor: String!
+}`;
+
+export const studentsQueries = `
+    allStudents: [Student]!
+    studentByCode(code: Int!): Student!
+`;
+
+export const studentsMutations = `
+    createStudent(student: StudentInput!): Student!
+    deleteStudent(code: Int!): Int
+    updateStudent(code: Int!, student: StudentInput!): Student!
+`;
